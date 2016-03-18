39674f228c7fe2774546c7ea02fbd818c102286f
Zeaa <fft1026@163.com>
2016-03-18 17:44:01 +0800

    1.0

---
+++ PolyvStreamerDemo/PolyvStreamerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PolyvStreamerDemo
+//
+//  Created by FT on 16/3/17.
+//  Copyright © 2016年 polyv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PolyvStreamerDemo/PolyvStreamerDemo/CameraViewController.h
@@ -0,0 +1,21 @@
+//
+//  CameraViewController.h
+//  PolyvStreamerDemo
+//
+//  Created by FT on 16/3/17.
+//  Copyright © 2016年 polyv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+
+@property (weak, nonatomic) IBOutlet UIView *previewView;
+
+@property (weak, nonatomic) IBOutlet UIButton *streamButton;
+
+@property (weak, nonatomic) IBOutlet UILabel *stateLabel;
+
+
+@end
