133e1f21a91b24e6d96fae8154c9cde13a88dab3
Zeaa <fft1026@163.com>
2016-03-25 14:54:25 +0800

    update

--- iOSInjectionProject/build/InjectionBundle.build/Debug-iphonesimulator/InjectionBundle.build/Objects-normal/x86_64/BundleContents.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/ft/Desktop/Git/PolyvStreamerDemo/iOSInjectionProject/BundleContents.m \
-  /tmp/injectionforxcode/BundleInjection.h \
-  /tmp/injectionforxcode/BundleInterface.h \
-  /Users/ft/Desktop/Git/PolyvStreamerDemo/iOSInjectionProject/BundleContents.h
